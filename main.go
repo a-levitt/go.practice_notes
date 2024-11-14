fad4ee1dea9171f001d53566bf5d5189248a569a
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-14 11:09:45 +0100

    Implement note module

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"alevitt.com/note/note"
+	"fmt"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+	var value string
+	fmt.Scanln(&value)
+
+	return value
+}
